81aa40026bcb067bfe837667d339ea813c79ffe6
afreen-dev <afreenmoham@gmail.com>
2026-01-17 17:29:06 +0530

    fixing application.properties

--- src/main/java/com/glowcart/api/controller/healthController.java
+++ src/main/java/com/glowcart/api/controller/healthController.java
@@ -1,15 +1,18 @@
 package com.glowcart.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class healthController {
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Glowcart Backend is running!";
+    }
 
     @GetMapping("/health")
     public String health() {
-        return "Glowcart Backend is running";
+        return "OK";
     }
 }
