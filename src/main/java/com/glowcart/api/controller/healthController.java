99d0e4191261a201f9c2afe7bdda32ffe0115c04
afreen-dev <afreenmoham@gmail.com>
2026-01-17 18:34:31 +0530

    t push origin main Adding healthcontroller.java

---
+++ src/main/java/com/glowcart/api/controller/healthController.java
@@ -0,0 +1,18 @@
+package com.glowcart.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Glowcart Backend is running!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
